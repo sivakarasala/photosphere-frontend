717fd2eddc1f62dabeacc5dc69c1c186edae0ac5
Siva Karasala <shivataraya@gmail.com>
2025-01-15 20:38:05 +0530

    Added a header to change between pages (with simple styling).

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import { GalleryPage } from "./pages/gallery";
 import { UploadPage } from "./pages/upload";
 
 export function App() {
     return (
        <BrowserRouter>
+            <div className="nav">
+                <div className="title">Photosphere</div>
+                <Link to="/" className="link">Home</Link>
+                <Link to="/upload" className="link">Upload</Link>
+            </div>
             <Routes>
                 <Route path="/" element={<GalleryPage />} />
                 <Route path="/upload" element={<UploadPage />} />
