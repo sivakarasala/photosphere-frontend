7673f1facf355a8927427fffa22d7f713f213c6f
Siva Karasala <shivataraya@gmail.com>
2025-01-15 20:23:23 +0530

    Remove BASE_URL access in App component.

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,6 @@
 import React from "react";
 import { GalleryPage } from "./pages/gallery";
 
-const BASE_URL = process.env.BASE_URL;
-if (!BASE_URL) {
-    throw new Error(`Set BASE_URL environment variable.`);
-}
-
-console.log(`Expecting backend at ${BASE_URL}.`);
-
 export function App() {
     return (
         <GalleryPage />
