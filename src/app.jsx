eee7ed270caea877ee6282b3395c56af0edc6ab7
Siva Karasala <shivataraya@gmail.com>
2025-01-15 20:00:36 +0530

    Requesting assets from the backend and logging to the console.

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,20 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+
 export function App() {
-    return <h1>Hello computer!</h1>;
+    
+    useEffect(() => {
+        axios.get(`http://localhost:3000/assets`)
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.log(`Error retreiving assets:`);
+                console.log(error);
+            });
+    }, []);
+
+    return (
+        <h1>Aum Namah Sivaya!</h1>
+    );
 }
